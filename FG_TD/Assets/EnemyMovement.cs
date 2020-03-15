83583014478832d2055f709f2d5afbf8ac81c568
Floggy <floglor033@gmail.com>
2020-03-15 13:08:48 +0200

    First commit

---
+++ FG_TD/Assets/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector2 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector2.Distance(transform.position, target.position) <= 0.04f)
+        {
+            GetNextWaypoint();
+        }
+
+
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        waypointIndex++;
+        target = Waypoints.points[waypointIndex];
+    }
+}

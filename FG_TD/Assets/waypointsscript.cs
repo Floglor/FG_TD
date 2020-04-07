a09c2b3016f6d818519f24bd45065170bf50892f
Zlovadik <Zlovadik@mail.ru>
2020-04-07 21:31:57 +0300

    up

---
+++ FG_TD/Assets/waypointsscript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypointsscript : MonoBehaviour
+{
+    public float range;
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
